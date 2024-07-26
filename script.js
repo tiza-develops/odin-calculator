551ae7aa32a1f98c653d980fec80482def5aff04
Tiza <tizadevel@tuta.io>
2024-07-26 10:55:44 -0600

    Design calculator layout and add a javascript file with pseudocode
    In this commit I finished the html and css that displays a calculator with buttons. Furthermore, I added a script.js file that incorporates pseudocode of what the program should do.

---
+++ script.js
@@ -0,0 +1,8 @@
+/*
+ here what the code will do:
+ 1.- Parse a string given by the calculator once the = button is pressed.
+ 2.- Break down the string into number, operand number.
+ 3.- Identify the operand and assign it to a mathematical operation.
+ 4. Perform the operation between the two numbers.
+ 5. Print out the result.
+ */

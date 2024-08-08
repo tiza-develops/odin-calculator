396a05b3ab4c8dd81369e43d60f98802b531d3f6
tiza <main@tiza.io>
2024-08-08 10:22:52 -0600

    Independent function that takes string and returns an object

--- operations.js
+++ operations.js
@@ -1,9 +1,11 @@
-function(string) {
-  let operation = {
-    operand1: "null",
-    type: "null",
-    operand2: "null",
-  }
-  return object
+function identification(string) {
+  let regexFetchNumbers = /[\+\-\*\/]/g;
+  let regexFetchSymbols = /[0-9]/g;
+  let objectComputation = {
+    numbers : string.split(regexFetchNumbers),
+    operations : string.split(regexFetchSymbols),
+    }
+  return objectComputation;
 }
 
+console.log(identification("5017*1+2-4/2"));

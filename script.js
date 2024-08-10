c0aeff2eb7d0233e7ef57c419f600f860ab7ca9d
tiza <main@tiza.io>
2024-08-10 12:19:37 -0600

    Add number class for every number button

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let buttons = document.querySelectorAll("button");
 buttons.forEach( button => {
     button.addEventListener('click',
     function(event) {
+      if (element.classlist.contains())
       let results = document.querySelector(".screen")
       results.innerHTML += event.target.innerHTML;
     }

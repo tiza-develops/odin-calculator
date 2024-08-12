2782584c63361466b26999982cdfecd6d308cf60
tiza <main@tiza.io>
2024-08-12 12:24:46 -0600

    Finally two different event handlers that work

--- script.js
+++ script.js
@@ -3,6 +3,19 @@
   input: button click (event)
   output: screen innerHTML
 */
+function input(number) {
+  let screenElement = document.querySelector(".screen");
+  screenElement.innerHTML += number.getAttribute("number");
+}
+
+const digitButtons = document.querySelectorAll(".clickable");
+digitButtons.forEach(digitButton => {
+  digitButton.addEventListener('click', () => {
+    // Passes a REFERENCE to the function, not the function,
+    // otherwise, it would be invoked at startime.
+    input(digitButton);
+  });
+});
 
 /*
   function 2
@@ -10,6 +23,21 @@
   output: object
 */
 
+function operate(string) {
+  // Declare regex
+  let reNum = /[0-9]/g;
+  let reOp = /[\+]/g;
+  // Put regex into variables
+  let numbers = string.match(reNum) || [];
+  let operators = string.match(reOp) || [];
+  alert(numbers);
+}
+
+let equalButton = document.querySelector(".equal");
+equalButton.addEventListener('click', () => {
+  let screen = document.querySelector(".screen");
+  operate(screen.innerHTML);
+});
 /*
   function 3
   input: function 2's object
